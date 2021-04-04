d97bac03aeb443bcf92ddb1f1146042816a02747
samuelvitry <51719625+samuelvitry@users.noreply.github.com>
2021-04-04 15:48:59 +0200

    login + retreive all event from user

--- Test backend/testcalendar/calendrier/models.py
+++ Test backend/testcalendar/calendrier/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
-class Utilisateur(models.Model):
-    public_name = models.CharField(max_length=128)
-    mail_adresse = models.EmailField(max_length=512)
-    mdp = models.CharField(max_length=512)
-    key1 = models.CharField(max_length=512)
-
 class Evenement(models.Model):
     event_name = models.CharField(max_length=512)
     start_date = models.DateTimeField('Start date')
     end_date = models.DateTimeField('End date')
-    proprio = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    proprio = models.CharField(max_length=512)
+    def __str__(self):
+        return self.event_name
--- Test backend/testcalendar/calendrier/urls.py
+++ Test backend/testcalendar/calendrier/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('login/', views.login_view, name='login_view'),
 ]

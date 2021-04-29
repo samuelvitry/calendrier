8af5e69364f0925d33d3d5ea678d4ed38524c10d
samuelvitry <51719625+samuelvitry@users.noreply.github.com>
2021-04-29 17:31:55 +0200

    Debut backend

---
+++ Backend/calendrier/api/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+import string
+import random
+
+def getCode():
+    length = 32
+
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if User.objects.filter(code=code).count() == 0:
+            break
+    
+    return code
+
+class User(models.Model):
+    email = models.EmailField(max_length=512, unique=True)
+    password = models.CharField(max_length=1024)
+    username = models.CharField(max_length=512, unique=True)
+    account_creation_date = models.DateTimeField(auto_now_add=True)
+    cle = models.CharField(max_length=1024)
+    code = models.CharField(max_length=32, default=getCode)
+    def __str__(self):
+        return self.username
+
+class Evenement(models.Model):
+    event_name = models.CharField(max_length=512)
+    start_date = models.IntegerField()
+    end_date = models.IntegerField()
+    full = models.BooleanField(default=True)
+    proprio = models.CharField(max_length=512)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    color = models.IntegerField(default=5)
+    def __str__(self):
+        return self.event_name
+

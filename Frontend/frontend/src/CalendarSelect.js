569eba1b62c896d8be0834a94b4afe75d8f974db
samuelvitry <51719625+samuelvitry@users.noreply.github.com>
2021-06-06 12:21:32 +0200

    Delete marche

--- Frontend/frontend/src/CalendarSelect.js
+++ Frontend/frontend/src/CalendarSelect.js
@@ -4,6 +4,27 @@ import { Checkbox } from './Checkbox'
 export const CalendarSelect = (props) => {
 
     console.log(props.stockageCalendar)
+    
+    if(props.stockageCalendar.length >= 1){
+        return (
+            <section className="calendar-select">
+                <h2>Selection des calendriers</h2>
+                <div className="container-select-calendar">
+                    {props.calendarList.map((x) => (<Checkbox checked={props.stockageCalendar[x][0]} txt={x} changement={() => props.calendarSelecSwitch(x)} />))}
+                </div>
+            </section>
+        )
+    }
+    else {
+        return (
+            <section className="calendar-select">
+                <h2>Selection des calendriers</h2>
+                <div className="container-select-calendar">
+                    <Checkbox checked={true} txt='Default Calendar' changement={() => {}}/>
+                </div>
+            </section>
+        )
+    }
 
     return (
         <section className="calendar-select">

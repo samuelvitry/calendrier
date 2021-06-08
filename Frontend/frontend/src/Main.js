cef03c9d4f1ffe37e2143b0f09ad00c649649b8c
samuelvitry <51719625+samuelvitry@users.noreply.github.com>
2021-06-08 14:51:07 +0200

    #11
    Dans traiter event, la start et end date est egale a elle même + le décalage de la timezone client

--- Frontend/frontend/src/Main.js
+++ Frontend/frontend/src/Main.js
@@ -103,6 +103,11 @@ export const Main = (props) => {
                     else {
                         tempSto[objCalName] = [true, tempEvents[i]]
                     }
+                    var TZoffset = new Date().getTimezoneOffset() * 60
+                    tempEvents[i]['start_date'] = tempEvents[i]['start_date'] + TZoffset
+                    tempEvents[i]['end_date'] = tempEvents[i]['end_date'] + TZoffset
+                    console.log(TZoffset)
+                    console.log(tempEvents[i]['start_date'])
                 }
                 if (tempSto.length < 1) {
                     tempSto['Default Calendar'] = [true]

27092fd62e0272010d12c6c4670366c49023b33a
samuelvitry <51719625+samuelvitry@users.noreply.github.com>
2021-06-14 23:30:40 +0200

    Debut du new calendar, commit for the day

--- Frontend/frontend/src/CalendarSelect.js
+++ Frontend/frontend/src/CalendarSelect.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Checkbox } from './Checkbox'
+import { useState } from 'react'
+import { Button } from './Button'
 
 export const CalendarSelect = (props) => {
+
+    const [isAdd, setIsAdd] = useState(false)
     
     if(Object.keys(props.stockageCalendar).length > 0){
         return (
@@ -16,8 +20,12 @@ export const CalendarSelect = (props) => {
     else {
         return (
             <section className="calendar-select">
-                <h2>Selection des calendriers</h2>
+                <div className='calendar-select-top'>
+                    <h2>Selection des calendriers</h2>
+                    <h2 onClick={() => (setIsAdd(true))} id='calendar-add'>+</h2>
+                </div>
                 <div className="container-select-calendar">
+                    {isAdd ? <div className='calendar-add-div'><input className='contained-input' placeholder={'Calendar name'}/><Button full txt={'Add'}/></div> : null}
                     <Checkbox checked={true} txt='Default Calendar' changement={() => {}}/>
                 </div>
             </section>

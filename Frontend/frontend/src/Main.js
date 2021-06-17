66f2f13995d14dbfaea6816e6e249dfbd1d8e439
samuelvitry <51719625+samuelvitry@users.noreply.github.com>
2021-06-17 18:00:44 +0200

    Event detail on next event

--- Frontend/frontend/src/Main.js
+++ Frontend/frontend/src/Main.js
@@ -272,7 +272,7 @@ export const Main = (props) => {
             <div className="left-section">
                 <MiniCalendar annim={annim} eventList={generateEventList()} isSele={isWeekly} month={month} year={year} week={week} nextMonth={() => nextMonth()} prevMonth={() => prevMonth()}/>
                 <CalendarSelect reload={() => forceReload()} stockageCalendar={stockageCalendar} calendarSelecSwitch={(x) => calendarSelecSwitch(x)} calendarList={generateCalendarTable()} ajouterEvent={(x) => ajouterEvent(x)}/>
-                <NextEvents eventList={eventList} />
+                <NextEvents eventList={eventList} reload={() => forceReload()}/>
             </div>
             <div className="right-section">
                 {isWeekly ? <WeeklyCalendar reload={() => forceReload()} setAnnim={(x) => setAnnim(x)} ajouterEvent={(x) => ajouterEvent(x)} calendarList={generateCalendarTable()} switch={() => switchMonWee()} nextWeek={() => nextWeek()} prevWeek={() => prevWeek()} year={year} week={week} month={month} eventList={generateWeeklyList()}/> : <MonthlyCalendar reload={() => forceReload()} setAnnim={(x) => setAnnim(x)} annim={annim} ajouterEvent={(x) => ajouterEvent(x)} switch={() => switchMonWee()} calendarList={generateCalendarTable()} nextMonth={() => nextMonth()} prevMonth={() => prevMonth()} month={month} year={year} eventList={generateEventList()}/>}
--- Frontend/frontend/src/NextEvents.js
+++ Frontend/frontend/src/NextEvents.js
@@ -1,4 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { api } from './Main'
+
+const monthConv = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+}
+
+const EventDetail = (props) => {
+
+    var name = props.event['event_name']
+    var date_debut = ''
+    var duration = ''
+    var repetition = ''
+
+    var durationT = props.event['end_date'] + 1 - props.event['start_date']
+    var debut = new Date(props.event['start_date'] * 1000)
+
+    function deleteEvent() {
+        api.get('eventDelete' + '?key=' + props.event['key']).then((response) => {if(response.status == 200) {props.closeDetail(); props.reload()}})
+    }
+
+    if (durationT >= 86400) {
+        var debutMo = monthConv[debut.getMonth() + 1]
+        var debutY = debut.getFullYear()
+        var debutD = debut.getDate()
+        date_debut = debutD + ', ' + debutMo + ', ' + debutY
+        duration = Math.floor(durationT/86400) + ' days'
+    }
+    else {
+        var durationH = Math.floor(durationT/3600)
+        var durationM = Math.floor((durationT- (3600 * durationH))/60)
+        if (durationM < 10){
+            durationM = '0' + durationM
+        }
+        duration = durationH + 'h' + durationM
+
+        var debutMo = monthConv[debut.getMonth() + 1]
+        var debutY = debut.getFullYear()
+        var debutD = debut.getDate()
+        var debutH = debut.getHours()
+        var debutM = debut.getMinutes()
+        if (debutM < 10){
+            debutM = '0' + debutM
+        }
+        date_debut = debutD + ', ' + debutMo + ', ' + debutY + ' at ' + debutH + 'h' + debutM
+    }
+
+    const [isDropdown, setisDropdown] = useState(false)
+    //faire une fonction qui gère l'appuie sur le bouton édit et delete
+
+    return (
+        <div className='detail-container'>
+            <div className='event-detail'>
+                <div className='detail-first-line'>
+                    <i className="fas fa-times" onClick={(e) => {props.closeDetail(); e.stopPropagation()}}></i>
+                    <h2 style={{color: props.event['color']}}>{name}</h2>
+                    <i className="fas fa-ellipsis-h" onClick={() => setisDropdown(isDropdown ? false : true)}></i>
+                </div>
+                {isDropdown ? <div className='detail-dropdown'>
+                    <div onClick={() => setisDropdown(false)} className='detail-drop-edit'><i className="fas fa-pen"></i>Edit</div>
+                    <div onClick={() => setisDropdown(false)} className='detail-drop-delete' onClick={() => deleteEvent()}><i className="fas fa-trash"></i>Delete</div>
+                </div> : null}
+                <div className='detail-line'>
+                    <i style={{color: props.event['color']}} className="fas fa-clock"></i>
+                    <p>{date_debut}</p>
+                </div>
+                <div className='detail-line'>
+                    <i style={{color: props.event['color']}} className="fas fa-hourglass"></i>
+                    <p>{duration}</p>
+                </div>
+                <div className='detail-line'>
+                    <i style={{color: props.event['color']}} className="fas fa-calendar"></i>
+                    <p>{props.event['calendar']}</p> 
+                </div>
+                {repetition !== '' ? 
+                <div className='detail-line'>
+                    <i className="fas fa-redo"></i>
+                    <p>{repetition}</p>
+                </div> : null}
+            </div>
+        </div>
+    )
+}
 
 export const NextEvents = (props) => {
 
@@ -35,6 +128,8 @@ export const NextEvents = (props) => {
     }
 
     const NextEventItem = (props) => {
+
+        const [isDetail, setIsDetail] = useState(false)
 
         function dateToTxt(nbr) {
             let date = new Date(nbr * 1000)
@@ -75,13 +170,17 @@ export const NextEvents = (props) => {
         }
 
         return (
-            <div className="next-event-item">
-                <div className='next-event-dot' style={{backgroundColor: props.event['color']}}></div>
-                <div className='next-event-text'>
-                    <p className='next-event-name'>{props.event['event_name']}</p>
-                    <p className='next-event-date'>{dateToTxt(props.event['start_date'])}</p>
+            <>
+                {isDetail ? <EventDetail event={props.event} reload={() => props.reload()} closeDetail={() => {setIsDetail(false)}}/> : null}
+                <div className="next-event-item" onClick={(e) => setIsDetail(true)}>
+                    <div className='next-event-dot' style={{backgroundColor: props.event['color']}}></div>
+                    <div className='next-event-text'>
+                        <p className='next-event-name'>{props.event['event_name']}</p>
+                        <p className='next-event-date'>{dateToTxt(props.event['start_date'])}</p>
+                    </div>
                 </div>
-            </div>
+            </>
+            
         )
     }
 
@@ -90,7 +189,7 @@ export const NextEvents = (props) => {
     return (
         <section className="next-event">
             <h2>Prochains évenements</h2>
-            {filteredList.map((x) => (<NextEventItem event={x}/>))}
+            {filteredList.map((x) => (<NextEventItem key={x['key']} reload={() => props.reload()} event={x}/>))}
             {filteredList.length < 1 ? 
             <div className='next-event-error'>
                 <p>Il n'y a rien par ici...</p>

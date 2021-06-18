bc02bebd332f365c1581d2cc5ade4ce516f13aed
samuelvitry <51719625+samuelvitry@users.noreply.github.com>
2021-06-18 13:16:04 +0200

    To desktop

--- Frontend/frontend/src/Today.js
+++ Frontend/frontend/src/Today.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export const Today = () => {
+export const Today = (props) => {
 
     const hoursList = [
         "00h00",
         "01h00",
+        "02h00",
         "03h00",
         "04h00",
         "05h00",
@@ -26,14 +27,51 @@ export const Today = () => {
         "21h00",
         "22h00",
         "23h00",
+        "24h00"
     ]
-
+    const deuxhoursList = [
+        "00h00",
+        "02h00",
+        "04h00",
+        "06h00",
+        "08h00",
+        "10h00",
+        "12h00",
+        "14h00",
+        "16h00",
+        "18h00",
+        "20h00",
+        "22h00",
+        "24h00"
+    ]
+    
+    const HourPoint = (props) => {
+        return (
+            <div className='hour-point'>
+                <p>{props.hour}</p>
+                <div className='hour-dot' />
+            </div>
+        )
+    }
+    
+    const EventPoint = (props) => {
+        return (
+            <div className='event-point'>
+                <div className='event-dot' />
+                <div className='event-point-info'>
+                    
+                </div>
+            </div>
+        )
+    }
+    
 
     return (
         <div className='today'>
             <h2>Today</h2>
             <div className='today-actual'>
-                
+                <div className='today-line' />
+                {deuxhoursList.map((x) => <HourPoint hour={x} />)}
             </div>
         </div>
     )

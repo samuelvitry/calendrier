9c58d34dc3c68ec2cab0f972392c379ff6c39b1e
samuelvitry <samuel.vitry@orange.fr>
2021-08-01 16:51:18 +0200

    suppression de old

--- Backend/Old/Api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import User, Evenement
-
-admin.site.register(User)
-admin.site.register(Evenement)
--- Backend/Old/Api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import eventList, logIn
-
-urlpatterns = [
-    path('eventList', eventList.as_view()),
-    path('login', logIn.as_view()),
-]

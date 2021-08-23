6f767b213fd3260e0eeb842dae880942113c840a
samuelvitry <samuel.vitry@orange.fr>
2021-08-23 12:05:03 +0200

    oublié d'add ça

--- Frontend/frontend/src/index.js
+++ Frontend/frontend/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
-import { CookiesProvider } from "react-cookie";
+// import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
